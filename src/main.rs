2c4eab81df9d7043c5232da42f201245ae58a66f
Vyacheslav Molodyakov <vrmolodyakov@mail.ru>
2022-09-05 23:05:54 +0300

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use std::ptr;
+use winapi::ctypes::c_void;
+
+use winapi::um::winuser::{SystemParametersInfoW, SPI_GETDESKWALLPAPER};
+
 fn main() {
-    println!("Hello, world");
+    let wallpaper_path = get_current().unwrap();
+    println!("{}",wallpaper_path);
+}
+
+fn get_current() ->Result<String,Box<dyn std::error::Error>>{
+    unsafe{
+        let buffer :[u16;260] = [0;260];
+        let successful = SystemParametersInfoW(
+            SPI_GETDESKWALLPAPER,
+            buffer.len() as u32,
+            buffer.as_ptr() as *mut c_void,
+            0,
+        ) == 1;
+        if successful {
+            let path = String::from_utf16(&buffer)?
+                // removes trailing zeroes from buffer
+                .trim_end_matches('\x00')
+                .into();
+            Ok(path)
+        } else {
+            Err(io::Error::last_os_error().into())
+        }
+
+    }
 }
